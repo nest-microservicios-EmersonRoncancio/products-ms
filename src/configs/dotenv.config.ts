da3a434884b41df6ae2a20b7c64a30b76375336a
Emerson Roncancio <roncancioavila@gmail.com>
2025-03-15 03:11:16 -0500

    Comunicacion de microservicios por medio de Nats

--- src/configs/dotenv.config.ts
+++ src/configs/dotenv.config.ts
@@ -5,11 +5,15 @@ const envVarsSchema = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const { error, value } = envVarsSchema.validate(process.env);
+const { error, value } = envVarsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -18,6 +22,7 @@ if (error) {
 interface Env {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string[];
 }
 
 const env: Env = value as Env;
@@ -25,4 +30,5 @@ const env: Env = value as Env;
 export const envs = {
   PORT: env?.PORT,
   DATABASE_URL: env?.DATABASE_URL,
+  NATS_SERVERS: env?.NATS_SERVERS,
 };
